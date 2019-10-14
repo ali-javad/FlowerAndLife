7e37b13a5ed6210ec9bb989278c07812119267ae
Ali_kermani <alikermani45@gmail.com>
2019-10-14 19:24:58 +0330

    new update

--- app/src/main/java/com/espinas/slideshow/MainActivity.java
+++ app/src/main/java/com/espinas/slideshow/MainActivity.java
@@ -1,15 +1,21 @@
 package com.espinas.slideshow;
 
 import android.content.Context;
+import android.os.Handler;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.TableLayout;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     ViewPager viewPager;
     SlideShow adapter;
+    int currentPage ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +28,27 @@ public class MainActivity extends AppCompatActivity {
         viewPager = (ViewPager) findViewById(R.id.viewPager_id);
         adapter = new SlideShow(this);
 
-
         viewPager.setAdapter(adapter);
+
+               // Auto start of viewpager
+        final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            public void run() {
+                if (currentPage == 4) {
+                    currentPage = 0;
+                }
+                viewPager.setCurrentItem(currentPage++, true);
+            }
+        };
+        Timer swipeTimer = new Timer();
+        swipeTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, 2500, 2500);
+
+
 
     }
 }
--- app/src/main/java/com/espinas/slideshow/SlideShow.java
+++ app/src/main/java/com/espinas/slideshow/SlideShow.java
@@ -2,6 +2,7 @@ package com.espinas.slideshow;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,10 +10,13 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+
+
 public class SlideShow extends PagerAdapter {
 
     private Context context;
     LayoutInflater inflater;
+
 
     public int[] images ={
             R.drawable.img1,
@@ -43,16 +47,17 @@ public class SlideShow extends PagerAdapter {
 
     @NonNull
     @Override
-    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+    public Object instantiateItem(@NonNull ViewGroup container, final int position) {
 
        inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
        View view = inflater.inflate(R.layout.slideshow,container,false);
 
-        ImageView img = (ImageView) view.findViewById(R.id.imageView_id);
+        final ImageView img = (ImageView) view.findViewById(R.id.imageView_id);
         img.setImageResource(images[position]);
 
+
         container.addView(view);
-        return view;
+       return view;
     }
 
     @Override
@@ -61,4 +66,17 @@ public class SlideShow extends PagerAdapter {
 
     }
 
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position)
+        {
+            case 0 : return "درباره ما";
+            case 1 : return "درختچه ها";
+            case 2 : return "گلهای آپارتمانی";
+            case 3 : return "علاقه مندی های";
+            case 4 : return "انواع خاک";
+            default: return "";
+        }
+    }
 }
