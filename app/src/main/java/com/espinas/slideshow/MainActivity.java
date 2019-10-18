b7077c6fb3d3813d10b8a61886b7dbf297e360ca
Ali_kermani <alikermani45@gmail.com>
2019-10-18 15:04:12 +0330

    new update for viewpager

--- app/src/main/java/com/espinas/slideshow/MainActivity.java
+++ app/src/main/java/com/espinas/slideshow/MainActivity.java
@@ -2,10 +2,12 @@ package com.espinas.slideshow;
 
 import android.content.Context;
 import android.os.Handler;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 import android.widget.TableLayout;
 
 import java.util.Timer;
@@ -30,14 +32,18 @@ public class MainActivity extends AppCompatActivity {
 
         viewPager.setAdapter(adapter);
 
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabLayout_id);
+        tabLayout.setupWithViewPager(viewPager);
+
+
        // Auto start of viewpager
         final Handler handler = new Handler();
         final Runnable Update = new Runnable() {
             public void run() {
-                if (currentPage == 4) {
-                    currentPage = 0;
-                }
-                viewPager.setCurrentItem(currentPage++, true);
+                    if (currentPage == 5) {
+                        currentPage = 0;
+                    }
+                    viewPager.setCurrentItem(currentPage++, true);
             }
         };
         Timer swipeTimer = new Timer();
